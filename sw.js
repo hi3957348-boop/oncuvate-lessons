9d5fba261edb8772ca5201656d7794eb140ffc92
hi3957348-boop <hi3957348@gmail.com>
2026-07-15 17:41:30 +0900

    앱 아이콘/스플래시 새 Oncuvate 로고로 교체, 스플래시 배경 흰색, 캐시 v3

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 온큐베이트 리딩 PWA 서비스워커 */
-const CACHE='oncuvate-v2';
+const CACHE='oncuvate-v3';
 const SHELL=['./app.html','./free.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
